045b9b50e8951681c0014ec598dcbe5408d1157e
VenesaOkuna <venesa.okuna@student.moringaschool.com>
2022-09-16 12:41:48 +0300

    add specific choices available on listing

---
+++ listings/choices.py
@@ -0,0 +1,72 @@
+bedroom_choices = {
+  '1':1,
+  '2':2,
+  '3':3,
+  '4':4,
+  '5':5,
+  '6':6,
+  '7':7,
+  '8':8,
+  '9':9,
+  '10':10
+  }
+
+county_choices = {
+  'Nairobi':'Nairobi',
+  'Kisumu':'Kisumu',
+  'Mombasa':'Mombasa',
+  'Nakuru':'Nakuru',
+  'Eldoret':'Eldoret',
+  'Homabay':'Homabay',
+
+  }
+
+
+price_choices = {
+  '5000':'ksh.5,000',
+  '6000':'Ksh.6,000',
+  '7000':'Ksh.7,000',
+  '8000':'Ksh.8,000',
+  '9000':'Ksh.9,000',
+  '10000':'Ksh.10,000',
+  '11000':'ksh.11,000',
+  '12000':'ksh12,000',
+  '13000':'ksh.13,000',
+  '14000':'ksh.14,000',
+  '15000':'Ksh.15,000',
+  '16000':'ksh.16,000',
+  '17000':'ksh17,000',
+  '18000':'ksh.18,000',
+  '19000':'ksh.19,000',
+  '20000':'Ksh.20,000',
+  '21000':'ksh.21,000',
+  '22000':'ksh22,000',
+  '123000':'ksh.23,000',
+  '24000':'ksh.24,000',
+}
+
+state_choices = {
+        
+       'AP': 'Baba Dogo',
+       'HYD' : 'Pangani' ,
+       'AP' : 'Ziwani',
+       'AS':'Juja',
+       'BH':'Eastleigh',
+       'P':'South C',
+       'CHG':'South B',
+       'Rp':"Lang'ata",
+       'G': 'Ngara',
+       'GU':'Umoja',
+       'H': 'Wangige',
+       'HP': 'Lower Kabete',
+       'JAK'  : 'Mowlem',
+       'JK': 'Kikuyu',
+       'RCH': 'Rongai',
+       'KR': 'Komarock',
+       'KRL' : 'Dandora',
+       'MP' : 'Highridge',
+       'BP' :  'Westlands',
+       'MP':'Nairobi West',
+       'OD' :  'Utawala'
+       
+}
